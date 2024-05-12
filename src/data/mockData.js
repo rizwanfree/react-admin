697bc376dbdc171aeda531be384241498fc2ee4e
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2024-05-12 14:06:09 +0500

    react-admin-dashboard

---
+++ src/data/mockData.js
@@ -0,0 +1,163 @@
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: "Jon Snow",
+        email: "jonsnow@gmail.com",
+        age: 35,
+        phone: "(665)121-5454",
+        access: "admin"
+    },
+    {
+        id: 2,
+        name: "Daenerys Targaryen",
+        email: "khaleesi@example.com",
+        age: 30,
+        phone: "(789)456-1234",
+        access: "user"
+    },
+    {
+        id: 3,
+        name: "Tyrion Lannister",
+        email: "imp@example.com",
+        age: 40,
+        phone: "(123)456-7890",
+        access: "admin"
+    },
+    {
+        id: 4,
+        name: "Arya Stark",
+        email: "arya@example.com",
+        age: 25,
+        phone: "(987)654-3210",
+        access: "user"
+    },
+    {
+        id: 5,
+        name: "Sansa Stark",
+        email: "sansa@example.com",
+        age: 28,
+        phone: "(555)555-5555",
+        access: "user"
+    },
+    {
+        id: 6,
+        name: "Cersei Lannister",
+        email: "cersei@example.com",
+        age: 45,
+        phone: "(666)666-6666",
+        access: "admin"
+    },
+    {
+        id: 7,
+        name: "Jaime Lannister",
+        email: "jaime@example.com",
+        age: 42,
+        phone: "(777)777-7777",
+        access: "user"
+    },
+    {
+        id: 8,
+        name: "Bran Stark",
+        email: "bran@example.com",
+        age: 24,
+        phone: "(888)888-8888",
+        access: "admin"
+    },
+    {
+        id: 9,
+        name: "Robb Stark",
+        email: "robb@example.com",
+        age: 32,
+        phone: "(999)999-9999",
+        access: "user"
+    },
+    {
+        id: 10,
+        name: "Petyr Baelish",
+        email: "littlefinger@example.com",
+        age: 50,
+        phone: "(111)222-3333",
+        access: "admin"
+    },
+    {
+        id: 11,
+        name: "Margaery Tyrell",
+        email: "margaery@example.com",
+        age: 30,
+        phone: "(444)555-6666",
+        access: "user"
+    },
+    {
+        id: 12,
+        name: "Jorah Mormont",
+        email: "jorah@example.com",
+        age: 45,
+        phone: "(777)888-9999",
+        access: "admin"
+    },
+    {
+        id: 13,
+        name: "Samwell Tarly",
+        email: "samwell@example.com",
+        age: 28,
+        phone: "(111)222-3333",
+        access: "user"
+    },
+    {
+        id: 14,
+        name: "Theon Greyjoy",
+        email: "theon@example.com",
+        age: 30,
+        phone: "(444)555-6666",
+        access: "admin"
+    },
+    {
+        id: 15,
+        name: "Sandor Clegane",
+        email: "hound@example.com",
+        age: 40,
+        phone: "(777)888-9999",
+        access: "user"
+    },
+    {
+        id: 16,
+        name: "Davos Seaworth",
+        email: "davos@example.com",
+        age: 50,
+        phone: "(111)222-3333",
+        access: "admin"
+    },
+    {
+        id: 17,
+        name: "Brienne of Tarth",
+        email: "brienne@example.com",
+        age: 35,
+        phone: "(444)555-6666",
+        access: "user"
+    },
+    {
+        id: 18,
+        name: "Gendry Baratheon",
+        email: "gendry@example.com",
+        age: 32,
+        phone: "(777)888-9999",
+        access: "admin"
+    },
+    {
+        id: 19,
+        name: "Melisandre",
+        email: "melisandre@example.com",
+        age: 500,
+        phone: "(111)222-3333",
+        access: "user"
+    },
+    {
+        id: 20,
+        name: "Varys",
+        email: "varys@example.com",
+        age: 55,
+        phone: "(444)555-6666",
+        access: "admin"
+    },
+
+]
